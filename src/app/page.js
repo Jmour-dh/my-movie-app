8da9342250663f18b5cf15824d0ad2b5c0e0b1f2
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-09-06 14:26:13 +0200

    genre

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import MediaCard from "@/components/media-card/MediaCard";
 import styles from "./page.module.css";
 import Popular from "@/components/popular/Popular";
+import Genres from "@/components/genres/Genres";
 
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <Popular />
+      <Genres/>
     </div>
   );
 }
