ef250a2f1ef8a136e6e024a5ab2aaae699678350
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-09-08 17:59:45 +0200

    movie credits

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,13 @@ import styles from "./page.module.css";
 import Popular from "@/components/popular/Popular";
 import Genres from "@/components/genres/Genres";
 
+export const revalide = 86400;
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <Popular />
-      <Genres/>
+      <Genres />
     </div>
   );
 }
